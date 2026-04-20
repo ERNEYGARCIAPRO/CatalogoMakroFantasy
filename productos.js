42bd2640fc77c6f72ae16cd542792c084e213407
erneyGarcia <erneydariogarcia@gmail.com>
2026-04-20 16:30:08 -0500

    ingresando productos primeros 3

--- productos.js
+++ productos.js
@@ -1,18 +1,45 @@
 // La variable 'listaProductos' debe ser global para que main.js la vea
 const listaProductos = [
     { 
-        id: 1, 
-        titulo: "Mascarilla Facial Centella", 
-        marca: "Bioaqua",
-        categoria: "Belleza",
-        precio: 2000, 
+        id: 1,
+        titulo:"Banana Dorada", 
+        marca: "Generico",
+        categoria: "Fantasia",
+        precio:"1400 c/u", 
         img: [
-            "imagenes/mascarilla-facial-bioaqua/6942349738423-mascarilla-facial-bioaqua-centella.png",
-            "imagenes/mascarilla-facial-bioaqua/6942349736704-mascarilla-facial-bioaqua-enzimas.png",
-            "imagenes/mascarilla-facial-bioaqua/6976504680052-mascarilla-facial-bioaqua-caviar.png",
+            "imagenes/Banana-Dorada-195-196-197_1.jpg",
+            "imagenes/Banana-Dorada-195-196-197_2.jpg",
+            "imagenes/Banana-Dorada-195-196-197_3.jpg",
+            "imagenes/Banana-Dorada-195-196-197_4.jpg",
         ],
-        desc: "Extracto de Centella Asiática que hidrata y nutre tu piel dejándola suave y tersa." 
+        desc: "Paquete por 12 unidades, vienen en varios diseños, motivos y colores"
     },
+    { 
+        id: 2, 
+        titulo: "Gancho De Ropa", 
+        marca: "Rio",
+        categoria: "Cacharreria",
+        precio:"3500 paq", 
+        img: [
+            "imagenes/Gancho-De-Ropa-Rio-932_1.jpg",
+            "imagenes/Gancho-De-Ropa-Rio-932_2.jpg",
+        ],
+        desc: "Gancho de ropa plastico colores surtidos, paquete por 20 unidades"
+    },
+    { 
+        id: 3,
+        titulo: "Caiman Flor Doble", 
+        marca: "Generico",
+        categoria: "Fantasia",
+        precio:" 2600 c/u", 
+        img: [
+            "imagenes/Caiman-Flor-Doble-372_1.jpg",
+            "imagenes/Caiman-Flor-Doble-372_2.jpg",
+            "imagenes/Caiman-Flor-Doble-372_3.jpg",
+        ],
+        desc: "Caiman motivo flor doble  colores tropicales surtidos paquete por 12 unidades"
+    },
+
   
     // Para agregar más, pon una coma después de la llave y copia el bloque
 ];
